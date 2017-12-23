f2a194ca5345bd7f95965baa214078fe121a02eb
StevenTheTrumble <steven.trumble@uoit.net>
2017-12-22 20:13:43 -0500

    test works

--- strassen_pure_GA/simple_GA/test.py
+++ strassen_pure_GA/simple_GA/test.py
@@ -1,28 +1,96 @@
-def rotl(num, bits):
-    bit = num & (1 << (bits-1))
-    num <<= 1
-    if(bit):
-        num |= 1
-    num &= (2**bits-1)
+import numpy as np
 
-    return num
+def create_sols2():
+    C1 = np.array([[1], [0], [0], [0],
+                   [0], [1], [0], [0],
+                   [0], [0], [0], [0],
+                   [0], [0], [0], [0]])
 
-def rotr(num, bits):
-    num &= (2**bits-1)
-    bit = num & 1
-    num >>= 1
-    if(bit):
-        num |= (1 << (bits-1))
+    C2 = np.array([[0], [0], [1], [0],
+                   [0], [0], [0], [1],
+                   [0], [0], [0], [0],
+                   [0], [0], [0], [0]])
 
-    return num
+    C3 = np.array([[0], [0], [0], [0],
+                   [0], [0], [0], [0],
+                   [1], [0], [0], [0],
+                   [0], [1], [0], [0]])
+
+    C4 = np.array([[0], [0], [0], [0],
+                   [0], [0], [0], [0],
+                   [0], [0], [1], [0],
+                   [0], [0], [0], [1]])
+
+    final_sol = np.concatenate((C1, C2, C3, C4), axis=1)
+    return final_sol
+
+def determine_fitness(value):
+    solution =create_sols2()
+    a = np.dot(value, value.T)
+    b = np.linalg.pinv(a)
+    c = np.dot(value.T, b)
+    d = np.dot(value.T, solution)
+    e = np.dot(c.T, d)
+    f = np.subtract(e, solution)
+    g = np.dot(f, f.T)
+    h = np.trace(g)
+    return 1/(1+h), d
 
 
-print rotl(255,8);
-print rotr(255,8);
-numr=1;
-numl=1;
-for i in range(0,10):
-    print "0x%02X (%d)" % (numr,numr)
-    print "0x%02X (%d)" % (numl,numl)
-    numr = rotr(numr,8) 
-    numl = rotl(numl,8)
+A =[[ 1,  0,  0,  1,  1, -1, -1],
+ [ 0, -1,  1, -1,  1,  0,  1],
+ [ 0,  1,  1,  1,  1,  0,  0],
+ [ 0,  1,  1,  1, -1,  0,  0],
+ [ 0, -1,  1, -1,  1,  0,  1],
+ [ 0,  0, -1,  1,  1, -1, -1],
+ [ 1,  0,  1,  0,  0,  0,  0],
+ [ 0,  1,  1,  1,  1,  0,  0],
+ [-1, -1, -1,  1, -1,  0,  1],
+ [-1,  0, -1,  0,  0,  0,  0],
+ [ 1,  0,  0, -1,  1, -1,  1],
+ [-1,  0,  1,  0,  0,  0,  1],
+ [ 1,  1,  0,  1, -1,  0, -1],
+ [ 0, -1,  0,  1, -1,  0,  1],
+ [ 0, -1,  1, -1,  1,  0,  1],
+ [ 1,  0,  0, -1,  1, -1,  1]]
+
+C1 = np.array([1,0, 0, 0,
+               0, 1, 0, 0,
+               0, 0, 0, 0,
+               0, 0,0, 0])
+
+C2 = np.array([0, 0, 1, 0,
+               0, 0, 0, 1,
+               0, 0, 0, 0,
+               0, 0, 0, 0])
+
+C3 = np.array([0, 0, 0, 0,
+               0, 0, 0, 0,
+               1, 0, 0, 0,
+               0, 1, 0, 0])
+
+C4 = np.array([0, 0, 0, 0,
+               0, 0, 0, 0,
+               0, 0, 1, 0,
+               0, 0, 0, 1])
+A = np.array(A)
+
+x1 = np.array(np.linalg.lstsq(A,C1))
+x2 = np.array(np.linalg.lstsq(A,C2))
+x3 = np.array(np.linalg.lstsq(A,C3))
+x4 = np.array(np.linalg.lstsq(A,C4))
+
+print x1
+#fitness, d = determine_fitness(A)
+final_sol=[]
+final_sol.append(x1[0].tolist())
+final_sol.append(x2[0].tolist())
+final_sol.append(x3[0].tolist())
+final_sol.append(x4[0].tolist())
+final_sol= np.array(final_sol)
+rounded = np.round(final_sol.T,2)
+doty = np.dot(A,final_sol.T)
+dotx = np.dot(A,rounded)
+print np.round(doty,10)
+print rounded
+
